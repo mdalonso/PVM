30acd2786d29454b0cf292d13c9d195dc2c9ca0c
mdalonso <38704285+mdalonso@users.noreply.github.com>
2024-12-15 20:12:41 +0100

    Update Player.cs

--- UD3/Player.cs
+++ UD3/Player.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
-public class Player : Character//, IInteractable
+public class Player : Character, IInteractable
 {
     public int strength = 8;
 
@@ -22,25 +22,25 @@ public class Player : Character//, IInteractable
     public override void ShowMessage()
     {
         Debug.Log("++++++++++++++++++ Informe de Player +++++++++++++++++++++++++++");
-        //El campo health est· definido como private, por tanto no podemos acceder directamente a este campo
-        //Debug.Log(this.health); //Esta lÌnea dar· un error porque health est· definida como private en la clase base
+        //El campo health est√° definido como private, por tanto no podemos acceder directamente a este campo
+        //Debug.Log(this.health); //Esta l√≠nea dar√° un error porque health est√° definida como private en la clase base
         Debug.Log("Mi nombre es: " + this.playerName);
         Debug.Log("Mi vida es: " + this.Health);
         Debug.Log("Mi velocidad es: " + this.speed);
-        //Como _level es PROTECTED sÌ es acceible desde la clase derivada
-        Debug.Log("Mi puntuaciÛn es: " + this._level);
+        //Como _level es PROTECTED s√≠ es acceible desde la clase derivada
+        Debug.Log("Mi puntuaci√≥n es: " + this._level);
         Debug.Log("Mi fuerza es: " + this.strength);
         Debug.Log("++++++++++++++++++ FIN DE INFORME DE PLAYER +++++++++++++++++++++++++++");
 
 
     }
-    //Se define el mÈtodo st·tico AddScore que permite aÒadir los puntos especificados al campo score.
+    //Se define el m√©todo st√°tico AddScore que permite a√±adir los puntos especificados al campo score.
    
 
-    //public void Interact()
-    //{
-    //    Debug.Log("Jugador interactuando");
-    //}
+    public void Interact()
+    {
+        Debug.Log("Jugador interactuando");
+    }
 
 
 }
