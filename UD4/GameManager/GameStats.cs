5c59523ad6fb373953cd47022c5030a081eaf8a4
mdalonso <38704285+mdalonso@users.noreply.github.com>
2025-03-18 20:52:35 +0100

    Add files via upload

--- UD4/GameManager/GameStats.cs
+++ UD4/GameManager/GameStats.cs
@@ -7,13 +7,22 @@ using UnityEngine;
 public class GameStats : ScriptableObject
 {
     [SerializeField] int _initialTime = 30;
+    [SerializeField] int _initialScore = 0;
     
 
     public int time = 30;
+    [Range(1,10)] public int difficulty =1;
+
+    [SerializeField] int _score = 0;//Acumula los puntos del player
+    [SerializeField] int _scorePoints = 100;
+
+    public int Score { get => _score; set => _score = value; }
+    public int ScorePoints { get => _scorePoints; set => _scorePoints = value; }
 
     public void ResetState()
     {
         time=_initialTime;
+        Score = _initialScore;
     }
 
 }
